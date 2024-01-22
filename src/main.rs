13943160fe023be98c6087db577568703c64c7ee
swims_hjkl <sumedhrao7@gmail.com>
2024-01-22 22:58:49 +0530

    merge sort

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::io;
 mod bubble_sort;
-mod selection_sort;
 mod insertion_sort;
+mod selection_sort;
+mod two_way_merge_sort;
 
 fn main() {
     // enum_example::main();
-    let mut input_numbers :Vec<f32> = get_array_input();
+    let mut input_numbers: Vec<f32> = get_array_input();
     println!("");
     let sort_type = get_sort_type_selection();
     println!("");
@@ -13,7 +14,8 @@ fn main() {
         "1" => bubble_sort::main(&mut input_numbers),
         "2" => selection_sort::main(&mut input_numbers),
         "3" => insertion_sort::main(&mut input_numbers),
-        _ => panic!("Something went wrong")
+        "4" => two_way_merge_sort::main(&mut input_numbers),
+        _ => panic!("Something went wrong"),
     };
     println!("");
     println!("sorted array: {:?}", input_numbers);
@@ -25,12 +27,14 @@ fn get_sort_type_selection() -> String {
     println!("1. bubble sort");
     println!("2. selection sort");
     println!("3. insertion sort");
-    io::stdin().read_line(&mut sort_type).expect("Something went wrong taking input");
+    println!("4. merge sort");
+    io::stdin()
+        .read_line(&mut sort_type)
+        .expect("Something went wrong taking input");
     sort_type
 }
 
 fn get_array_input() -> Vec<f32> {
-
     println!("Enter the numbers spereated by ,");
     let mut input_numbers = String::new();
     match io::stdin().read_line(&mut input_numbers) {
@@ -41,11 +45,9 @@ fn get_array_input() -> Vec<f32> {
     let input_numbers: Vec<f32> = input_numbers
         .split(",")
         .into_iter()
-        .filter_map(|elem| {
-            match elem.trim().parse::<f32>() {
-                Ok(val) => Some(val),
-                Err(_) => None,
-            }
+        .filter_map(|elem| match elem.trim().parse::<f32>() {
+            Ok(val) => Some(val),
+            Err(_) => None,
         })
         .collect();
     input_numbers
---
+++ src/two_way_merge_sort.rs
@@ -0,0 +1,42 @@
+pub fn main(array: &mut Vec<f32>) -> &Vec<f32> {
+    merge_sort(array).expect("something went wrong")
+}
+
+fn merge_sort(a: &mut Vec<f32>) -> Option<&Vec<f32>> {
+    if a.len() > 1 {
+        let mid: usize = a.len() / 2;
+        let mut left: Vec<f32> = a[0..mid].to_vec();
+        let mut right: Vec<f32> = a[mid..a.len()].to_vec();
+        merge_sort(&mut left);
+        merge_sort(&mut right);
+        Some(merge(a, &left, &right))
+    } else {
+        return None;
+    }
+}
+
+fn merge<'a>(array: &'a mut Vec<f32>, a1: &Vec<f32>, a2: &Vec<f32>) -> &'a Vec<f32> {
+    let (mut i, mut j, mut k) = (0, 0, 0);
+    while i < a1.len() && j < a2.len() {
+        if a1[i] < a2[j] {
+            array[k] = a1[i];
+            i = i + 1;
+            k = k + 1;
+        } else {
+            array[k] = a2[j];
+            j = j + 1;
+            k = k + 1;
+        }
+    }
+    while i < a1.len() {
+        array[k] = a1[i];
+        i = i + 1;
+        k = k + 1;
+    }
+    while j < a2.len() {
+        array[k] = a2[j];
+        k = k + 1;
+        j = j + 1;
+    }
+    array
+}
